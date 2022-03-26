b3a767d1036f18be458e4f1ad77e2e982361d982
leminhthanh142 <thanhle12345789@gmail.com>
2022-03-26 21:27:26 +0700

    update pages

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { CommonLayout } from '../../components/layout/common';
 import './style.css';
+import { Link } from 'react-router-dom';
+import { Box, Button, Typography } from '@mui/material';
 
 export const HomePage = () => {
   return (
     <CommonLayout>
       <div>Home Page</div>
+      <Box mb={15}>
+        <Link to={'/tutorials'}>
+          <Button variant={'contained'}>
+            <Typography fontSize={'18px'}>Khám phá thêm các bài giảng thôi nào</Typography>
+          </Button>
+        </Link>
+      </Box>
     </CommonLayout>
   );
 };
